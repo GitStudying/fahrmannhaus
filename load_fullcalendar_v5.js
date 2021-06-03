13dee67d326f09148637f9d72b1247d02bc462a9
Hermens <p.e.hermens@student.tue.nl>
2021-06-03 12:17:43 +0200

    Update load_fullcalendar_v5.js

--- load_fullcalendar_v5.js
+++ load_fullcalendar_v5.js
@@ -5,7 +5,9 @@ var pageAvailability = false;
 var pageInfo = false;
 var pageHome = true;
 
-if(window.location.href.indexOf("vakantiewoning") > -1)
+if(window.location.href.indexOf("editor") > -1)
+	page="editor";
+else if(window.location.href.indexOf("vakantiewoning") > -1)
 	noheader= true;
 else if(window.location.href.indexOf("tarieven") > -1) 
 {
@@ -30,22 +32,43 @@ var calendarEl;
 var calendarjson;
 
 document.addEventListener('DOMContentLoaded', function() {
-	$(".banner").parent().slideUp('slow');
-	/*if(noheader)
-	{
-		$(".banner").parent().slideUp('slow');
-	} else {
-		$(".banner").parent().fadeIn('slow');
-	}*/
-	
-
-	if(!(pageAvailability || pageInfo))
-		return;
-	fullcalendar_init();
+	if(pageAvailability || pageInfo)
+		fullcalendar_init();
+	else if(page == "editor")
+		document.getElementById('fullcalendar-editor').innerHTML = "Je bent in de editor, ga naar de site om de kalender te zien" ;
 });
 
 // Only called when either availability or info
 function fullcalendar_init() {
+	// Add necessary scripts
+			/*var script_tag = document.createElement('script');
+		script_tag.setAttribute("type","text/javascript");
+		script_tag.setAttribute("src","https://kit.fontawesome.com/543a1e5fb2.js");
+		(document.getElementsByTagName("head")[0] || document.documentElement).appendChild(script_tag);*/
+	
+	/*var scripts  = ["https://cdn.jsdelivr.net/gh/PaulHermens/fahrmannhaus/fullcalendar-4.1.0/packages/core/main.min.js","https://cdn.jsdelivr.net/gh/PaulHermens/fahrmannhaus/fullcalendar-4.1.0/packages/daygrid/main.min.js","https://cdn.jsdelivr.net/gh/PaulHermens/fahrmannhaus/fullcalendar-4.1.0/packages/list/main.min.js","https://cdn.jsdelivr.net/gh/PaulHermens/fahrmannhaus/fullcalendar-4.1.0/packages/google-calendar/main.min.js",
+	"https://cdn.jsdelivr.net/gh/PaulHermens/fahrmannhaus/fullcalendar-4.1.0/packages/core/locales/nl.js"];
+	
+	for(var i = 0; i <= scripts.length-1; i++){
+		var script_tag = document.createElement('script');
+		script_tag.setAttribute("type","text/javascript");
+		script_tag.setAttribute("src",scripts[i]);
+script_tag.async = false; 
+		(document.getElementsByTagName("head")[0] || document.documentElement).appendChild(script_tag);
+	}
+	
+	// Add necessary styles
+	var styles = ["https://cdn.jsdelivr.net/gh/PaulHermens/fahrmannhaus/fullcalendar-4.1.0/packages/core/main.min.css","https://cdn.jsdelivr.net/gh/PaulHermens/fahrmannhaus/fullcalendar-4.1.0/packages/daygrid/main.min.css","https://cdn.jsdelivr.net/gh/PaulHermens/fahrmannhaus/fullcalendar-4.1.0/packages/list/main.min.css"]
+	
+	for(var i = 0; i <= styles.length-1; i++){
+		var link_tag = document.createElement('link');
+		link_tag.setAttribute("rel","stylesheet");
+		link_tag.setAttribute("href",styles[i]);
+link_tag.async = false; 
+		(document.getElementsByTagName("head")[0] || document.documentElement).appendChild(link_tag);
+	}
+	*/
+
 	var googlecalapi = 'AIzaSyDK4W4kNWfk2QUTu4izsTaDlH4GYZ-OMy8';
 	
 	var availabilityID = 'jk5c8vi00vfhshlodq67ca7qq8@group.calendar.google.com';
@@ -54,30 +77,31 @@ function fullcalendar_init() {
 	
 	
 	calendarjson = {
-		headerToolbar: { start: 'prev,next today', center: 'title', end: 'dayGridMonth,listYear' }, // buttons for switching between views
-		
-		plugins: [ 'dayGrid', 'list', 'googleCalendar' ],
-		initialView: 'dayGridMonth',
-
+		defaultView: 'dayGridMonth',
+		header: {
+			left: 'prev,next today',
+			center: 'title',
+			right: 'dayGridMonth,listYear'
+		},
 		locale: 'nl',
 		displayEventTime: false, // don't show the time column in list view
 		googleCalendarApiKey: googlecalapi,
 		height: "auto"
 	};
 	
-	if($(window).width() < 1100){
-		calendarjson.initialView = 'listYear';
-		calendarjson.headerToolbar = {
+		   if(window.innerWidth < 1100){
+				  calendarjson.defaultView = 'listYear';
+				  calendarjson.header = {
 			left: '',
 			center: 'title',
 			right: 'dayGridMonth,listYear'
 		}
-		calendarjson.footerToolbar =  {
+					calendarjson.footer =  {
 			left: '',
 			center: 'prev,next,today',
 			right: ','
 		}
-	}
+		   }
 	
 	
 	if(pageAvailability) {	
@@ -93,24 +117,20 @@ function fullcalendar_init() {
 	// Create calendar itself
 	calendar = new FullCalendar.Calendar(calendarEl, calendarjson);
 	
-	document.getElementById('fullcalendar-editor').style.display = "none";
-	
 	// Render previously initiated calendars
-	calendar.render();
+	render();
 }
 
 function render() {
-	// Create calendar itself
-	calendar = new FullCalendar.Calendar(calendarEl, calendarjson);
-	
 	document.getElementById('fullcalendar-editor').style.display = "none";
 	
 	// Render previously initiated calendars
 	calendar.render();
 
-	if (calendar == null) {
-		setTimeout(function() { wait() }, 500);
+	if ($('#availability-ediger').children().length == 0) {
+		console.log('retrying fullcalendar')
+		// setTimeout(render(), 500);
 	} else {
-		//
+		console.log('fullcalendar rendered')
 	}
 }
